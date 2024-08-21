e60bf72b09aabd46d278d363766b35c6eceb2178
Olek_Olegovich <olek.olegovich@gmail.com>
2024-08-21 23:51:45 +0300

    Fixed typo

--- plugins/Phileas_PartyFormationController.js
+++ plugins/Phileas_PartyFormationController.js
@@ -3,6 +3,7 @@
 //=============================================================================
 // [Update History]
 // 2024.April.13 Ver1.0.0 First Release
+// 2024.August.21 Ver1.0.1 Fixed typo
 
 /*:
  * @target MZ
@@ -181,7 +182,7 @@
     
     const Origin_isCurrentItemEnabled= Window_MenuStatus.prototype.isCurrentItemEnabled;
     Window_MenuStatus.prototype.isCurrentItemEnabled = function() {
-        if ((enable || enableSwitch > 0 && $gameSwitches.value(movementSwitch) == true)
+        if ((enable || enableSwitch > 0 && $gameSwitches.value(enableSwitch) == true)
             && lockedPositions.includes(this.index() + 1) === true) {
             
             SoundManager.playBuzzer();
